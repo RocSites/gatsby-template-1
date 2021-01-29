779c005b6d1d9834d2b6f3747bbc8c11b5139f46
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2021-01-28 23:46:09 -0500

    added nav and gradient

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,35 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import RedPinLogo from '../images/vector/default-monochrome-black.svg'
+import {makeStyles} from '@material-ui/core/styles'
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `red`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
+const withStyles = makeStyles((theme) => ({
+  logo: {
+    height: "auto",
+    width: "170px",
+    margin: "20px"
+  },
+  navbar: {
+    background: "rgb(253,29,29)",
+    background: "linear-gradient(90deg, rgba(253,29,29,1) 0%, rgba(255,123,14,1) 48%, rgba(255,213,42,0.9640231092436975) 100%)"
+
+  }
+
+}));
+
+const Header = ({ siteTitle }) => {
+
+  const classes = withStyles();
+
+  return (
+    <header className={classes.navbar}>
+        <Link to="/"style={{color: `white`,textDecoration: `none`,}}>
+          <img className={classes.logo} src={RedPinLogo} alt="redpin ventures logo" />  
         </Link>
-      </h1>
-    </div>
-  </header>
-)
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,29 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import {makeStyles} from '@material-ui/core/styles'
 
-const IndexPage = () => (
-  <Layout>
+const withStyles = makeStyles((theme) => ({
+  root: {
+    background: "rgb(253,29,29)",
+    minHeight: "100vh",
+    background: "linear-gradient(90deg, rgba(253,29,29,1) 0%, rgba(255,123,14,1) 48%, rgba(255,213,42,0.9640231092436975) 100%)"
+  }
+
+}));
+
+const IndexPage = () => {
+  const classes = withStyles();
+
+  return (
+    <Layout>
     <SEO title="Home" />
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-     empty div
+    <div className={classes.root}>
+     {/* <Image/> */}
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
   </Layout>
-)
+  )
+}
 
 export default IndexPage
