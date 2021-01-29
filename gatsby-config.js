e52f6b4781aedce43255b23ff924e63fb4921bca
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2021-01-28 19:11:22 -0500

    minor color changes and routing

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `RedPin Ventures LLC`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `RedPin Ventures`,
+    description: `RedPin Ventures is a marketing and web development company focused on providing a top-tier customer experience`,
+    author: `Doug Kellermeyer`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
