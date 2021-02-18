f1d1b7fe713d0df686b8f9a0966f33d6f63ebc35
Douglas Kellermeyer <dkellermeyer@office.mindex.com>
2021-02-18 02:22:21 -0500

    removed vh height

--- src/pages/index.js
+++ src/pages/index.js
@@ -142,7 +142,7 @@ const withStyles = makeStyles((theme) => ({
     marginBottom: "20vh",
     "@media (max-width:960px)":{
       flexDirection: "column",
-      marginBottom: "20vh"
+      marginBottom: "150px"
     }
   },
   laptopMobileHeaderWrapper: {
